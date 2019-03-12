b17be72940163831f9d9a618b7c68674f145416e
root <root@Xiaomi3G.lan>
2019-03-12 22:26:11 +0100

    Added FirmwareUpgrade.sh

---
+++ FirmwareUpgrade.sh
@@ -0,0 +1,10 @@
+echo installing wget
+opkg install ca-bundles openssl-util ca-certificates libustream-openssl wget
+echo "enter the downloadlink"
+read DOWNLOAD_LINK
+echo "enter sha256sums file"
+read SHA256SUMS
+filename=$(wget $DOWNLOAD_LINK -nv 2>&1 |cut -d\" -f2)
+cd /tmp;wget $DOWNLOAD_LINK;wget $SHA256SUMS;sha256sum -c sha256sums 2>/dev/null|if grep OK; then sysupgrade -v $filename
+else echo "sum is not correct"
+fi
