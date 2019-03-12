e7072c7faedee6a3027fd7285a959a7cf6890d80
root <root@Xiaomi3G.lan>
2019-03-12 22:45:52 +0100

    Deleted git-http; Added installation of wget ssl

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -1,12 +1,10 @@
 opkg update
-opkg install ca-certificates
+opkg install ca-bundle openssl-util ca-certificates libustream-openssl wget
 opkg list-installed | grep -q uclient-fetch || opkg install uclient-fetch
 opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
 echo -e -n 'untrusted comment: LEDE usign key of Stan Grishin\nRWR//HUXxMwMVnx7fESOKO7x8XoW4/dRidJPjt91hAAU2L59mYvHy0Fa\n' > /tmp/stangri-repo.pub && opkg-key add /tmp/stangri-repo.pub
 ! grep -q 'stangri_repo' /etc/opkg/customfeeds.conf && echo 'src/gz stangri_repo https://raw.githubusercontent.com/stangri/openwrt-repo/master' >> /etc/opkg/customfeeds.conf
 opkg update
-opkg remove git
-opkg install git-http ca-bundle
 git clone git://github.com/tavinus/opkg-upgrade.git
 cd opkg-upgrade
 ./opkg-upgrade.sh -i
