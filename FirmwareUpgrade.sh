013ea02aaafed634e25aa2fb4805518ef3066b2d
dmuiX <no_c0t@posteo.org>
2019-07-22 19:28:31 +0200

    Update FirmwareUpgrade.sh

--- FirmwareUpgrade.sh
+++ FirmwareUpgrade.sh
@@ -5,6 +5,9 @@ read DOWNLOAD_LINK
 echo "enter sha256sums file"
 read SHA256SUMS
 filename=$(wget $DOWNLOAD_LINK -nv 2>&1 |cut -d\" -f2)
-cd /tmp;wget $DOWNLOAD_LINK;wget $SHA256SUMS;sha256sum -c sha256sums 2>/dev/null|if grep OK; then sysupgrade -v $filename
-else echo "sum is not correct"
-fi
+cd /tmp;wget $DOWNLOAD_LINK;
+wget $SHA256SUMS
+sha256sum -c sha256sums 2>/dev/null | if grep OK; then sysupgrade -v $filename; else echo "sum is not correct"; fi
+opkg update
+opkg install luci luci-ssl
+service uhttpd restart
