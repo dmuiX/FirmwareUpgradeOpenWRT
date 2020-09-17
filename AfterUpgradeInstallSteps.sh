2672e7326aa8e761477ff66834a7d5953fb397d2
dmuiX <no_c0t@posteo.org>
2020-09-17 21:47:44 +0200

    Update AfterUpgradeInstallSteps.sh

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -2,6 +2,8 @@ opkg update
 opkg install ca-bundle openssl-util ca-certificates wget git
 opkg list-installed | grep -q uclient-fetch || opkg install uclient-fetch
 opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
+wget --no-check-certificate https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk
+opkg install luci-theme-argon*.ipk
 opkg update
 git clone git://github.com/tavinus/opkg-upgrade.git
 cd opkg-upgrade
