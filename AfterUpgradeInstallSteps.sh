e67ea2f3ff7b74affdde86167d92ca62bfb113e5
dmuiX <store@posteo.de>
2021-04-04 23:30:22 +0200

    Install argon config

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -5,5 +5,7 @@ opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
 wget --no-check-certificate https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk
 opkg install luci-theme-argon*.ipk
 opkg update
-opkg install luci luci-ssl
+opkg install luci luci-ssl luci-compat luci-app-sqm
+wget https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.8-beta/luci-app-argon-config_0.8-beta_all.ipk
+opkg install luci-app-argon-config_0.8-beta_all.ipk
 service uhttpd restart
