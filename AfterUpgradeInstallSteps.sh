8c6c5043d4d494f61ef42d572025d071fb822472
dmuiX <store@posteo.de>
2021-04-04 23:16:23 +0200

    Deleted opkg-upgrade
    Does not make sense to upgrade single packages on openwrt as it might break functionality. Upgrades for packages come with systemupgrades

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -5,11 +5,5 @@ opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
 wget --no-check-certificate https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk
 opkg install luci-theme-argon*.ipk
 opkg update
-git clone git://github.com/tavinus/opkg-upgrade.git
-cd opkg-upgrade
-./opkg-upgrade.sh -i # installs the opkg-upgrade to /usr/sbin/opkg-upgrade
-cd .. 
-rm -r opkg-upgrade
-opkg-upgrade -f
 opkg install luci luci-ssl
 service uhttpd restart
