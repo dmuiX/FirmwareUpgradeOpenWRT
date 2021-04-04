bc730a7b974ca9ce5a96815b54a7a5b0acf46065
dmuiX <store@posteo.de>
2021-04-04 23:52:11 +0200

    Backup is not working so I will delete it

--- FirmwareUpgrade.sh
+++ FirmwareUpgrade.sh
@@ -8,5 +8,4 @@ read SHA256SUMS
 filename=$(wget $DOWNLOAD_LINK -nv 2>&1 |cut -d\" -f2)
 cd /tmp;wget $DOWNLOAD_LINK;
 wget $SHA256SUMS
-sysupgrade -b backup.tar.gz
-sha256sum -c sha256sums 2>/dev/null | if grep OK; then sysupgrade -f backup.tar.gz -v $filename; else echo "sum is not correct"; fi
+sha256sum -c sha256sums 2>/dev/null | if grep OK; then sysupgrade -v $filename; else echo "sum is not correct"; fi
