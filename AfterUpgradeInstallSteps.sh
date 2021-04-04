db3e2418ba311f80eb58989a95bc1574faacc705
dmuiX <store@posteo.de>
2021-04-04 23:54:34 +0200

    Update AfterUpgradeInstallSteps.sh

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -5,7 +5,7 @@ opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
 wget --no-check-certificate https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk
 opkg install luci-theme-argon*.ipk
 opkg update
-opkg install luci luci-ssl luci-compat luci-app-sqm
+opkg install luci luci-ssl luci-compat luci-app-sqm luci-lib-ipkg
 wget https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.8-beta/luci-app-argon-config_0.8-beta_all.ipk
 opkg install luci-app-argon-config_0.8-beta_all.ipk
 rm luci-theme-argon_2.2.5-20200914_all.ipk luci-app-argon-config_0.8-beta_all.ipk openwrt-19.[0-9][0-9].[0-9]-ramips-mt7621-xiaomi_mir3g-squashfs-sysupgrade.bin
