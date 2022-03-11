da8d5a29082ebc8698c3c06786d95788d0c9b94c
dmuiX <store@posteo.de>
2022-03-11 19:35:40 +0100

    Update AfterUpgradeInstallSteps.sh
    - added default values for the argon theme and the argon config
    - deleted not working regex
    - added openssh-sftp-server

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -1,15 +1,20 @@
 opkg update
-opkg install ca-bundle openssl-util ca-certificates wget git
+opkg install ca-bundle openssl-util ca-certificates wget luci luci-ssl luci-compat luci-app-sqm luci-lib-ipkg openssh-sftp-server
 opkg list-installed | grep -q uclient-fetch || opkg install uclient-fetch
 opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
-wget --no-check-certificate https://github.com/jerrykuku/luci-theme-argon/releases/download/v[0-9.]/luci-theme-argon_[0-9-.]*_all.ipk
-opkg install luci-theme-argon*.ipk
-opkg update
-opkg install luci luci-ssl luci-compat luci-app-sqm luci-lib-ipkg
-# echo "Enter the download link of the luci app argon config"
-# read LUCI_APP_ARGON_CONFIG
-# wget  $LUCI_APP_ARGON_CONFIG -O 
-wget https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.8-beta/luci-app-argon-config_[0-9.]*-beta_all.ipk
-opkg install luci-app-argon-config_[0-9.]*-beta_all.ipk
-rm luci-theme-argon_2.2.5-20200914_all.ipk luci-app-argon-config_0.8-beta_all.ipk openwrt-[0-9][0-9].[0-9][0-9].[0-9]-ramips-mt7621-xiaomi_mir3g-squashfs-sysupgrade.bin openwrt-[0-9][0-9].[0-9][0-9].[0-9]-ath79-generic-ubnt_unifiac-lr-squashfs-sysupgrade.bin
-service uhttpd restart
+
+cd /tmp
+read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk]"
+LUCI_APP_ARGON_CONFIG_LINK=${LUCI_APP_ARGON_CONFIG_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk}
+LUCI_APP_ARGON_CONFIG_FILENAME=$(echo $LUCI_APP_ARGON_CONFIG_LINK | cut -d/ -f9)
+
+wget $LUCI_APP_ARGON_CONFIG_LINK -O $LUCI_APP_ARGON_CONFIG_FILENAME
+opkg install $LUCI_APP_ARGON_CONFIG_FILENAME
+
+read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-theme-argon_2.2.9-20211016-1_all.ipk]"
+LUCI_ARGON_THEME_LINK=${LUCI_ARGON_THEME_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-theme-argon_2.2.9-20211016-1_all.ipk}
+LUCI_ARGON_THEME_FILENAME=$(echo $LUCI_ARGON_THEME_LINK | cut -d/ -f9)
+wget $LUCI_ARGON_THEME_LINK -O $LUCI_ARGON_THEME_FILENAME
+opkg install $LUCI_ARGON_THEME_FILENAME
+
+rm $LUCI_APP_ARGON_CONFIG_FILENAME $LUCI_ARGON_THEME_FILENAME
