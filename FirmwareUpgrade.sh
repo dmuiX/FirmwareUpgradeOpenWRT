d0a265cf2d866436323fd014ae40eddd32d3071f
dmuiX <store@posteo.de>
2022-03-11 01:30:22 +0100

    Update FirmwareUpgrade.sh

--- FirmwareUpgrade.sh
+++ FirmwareUpgrade.sh
@@ -5,7 +5,9 @@ echo "enter the downloadlink"
 read DOWNLOAD_LINK
 echo "enter sha256sums file"
 read SHA256SUMS
-filename=$(wget $DOWNLOAD_LINK -nv 2>&1 |cut -d\" -f2)
+echo "enter the backup file"
+read BACKUPFILE
+FILENAME=$(wget $DOWNLOAD_LINK -nv 2>&1 |cut -d\" -f2)
 cd /tmp;wget $DOWNLOAD_LINK;
 wget $SHA256SUMS
-sha256sum -c sha256sums 2>/dev/null | if grep OK; then sysupgrade -v $filename; else echo "sum is not correct"; fi
+sha256sum -c sha256sums 2>/dev/null | if grep OK; then sysupgrade -c -o -v -k -f $BACKUPFILE $FILENAME; else echo "sum is not correct"; fi
