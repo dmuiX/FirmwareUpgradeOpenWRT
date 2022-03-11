d8a72b8cd48ee26562a5b1cffe74c0a8b9f06240
dmuiX <store@posteo.de>
2022-03-11 19:36:30 +0100

    Update FirmwareUpgrade.sh

--- FirmwareUpgrade.sh
+++ FirmwareUpgrade.sh
@@ -1,20 +1,13 @@
-# echo "installing wget and some other stuff"
-# opkg install openssl-util ca-certificates wget
-# opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
-
-# echo "enter the downloadlink"
-# read DOWNLOAD_LINK
+#!/bin/bash
+echo "installing wget and some other stuff"
+opkg install openssl-util ca-certificates wget
+opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
 
 # UBNT_AP: https://downloads.openwrt.org/releases/21.02.2/targets/ath79/generic/openwrt-21.02.2-ath79-generic-ubnt_unifiac-lr-squashfs-sysupgrade.bin
+# https://downloads.openwrt.org/releases/21.02.2/targets/ath79/generic/sha256sums
 
 # Mi Router 3G: https://downloads.openwrt.org/releases/21.02.2/targets/ramips/mt7621/openwrt-21.02.2-ramips-mt7621-xiaomi_mi-router-3g-squashfs-sysupgrade.bin
-
-# echo "enter sha256sums file"
-# read SHA256SUMS
-
-# Mi Router 3G: https://downloads.openwrt.org/releases/21.02.2/targets/ramips/mt7621/sha256sums
-
-# Ubnt Router: https://downloads.openwrt.org/releases/21.02.2/targets/ath79/generic/sha256sums
+# https://downloads.openwrt.org/releases/21.02.2/targets/ramips/mt7621/sha256sums
 
 cd /tmp
 echo "create a backup file"
@@ -30,11 +23,9 @@ DOWNLOAD_LINK=${DOWNLOAD_LINK:-https://downloads.openwrt.org/releases/21.02.2/ta
 wget $DOWNLOAD_LINK
 FILENAME=$(echo $DOWNLOAD_LINK | cut -d/ -f9)
 
-# echo $FILENAME
-
 read -p "Enter the link for the sha256sum file [https://downloads.openwrt.org/releases/21.02.2/targets/ath79/generic/sha256sums]: " SHA256SUMS_LINK
 SHA256SUMS_LINK=${SHA256SUMS_LINK:-https://downloads.openwrt.org/releases/21.02.2/targets/ath79/generic/sha256sums}
 
 wget $SHA256SUMS_LINK
 SHA256SUMS=$(echo $SHA256SUMS_LINK | cut -d/ -f9)
-sha256sum -c $SHA256SUMS 2>/dev/null | if grep OK; then sysupgrade -T -f $BACKUPFILE $FILENAME 2>&1 > error.log; if [[ $? -eq 0 ]]; then sysupgrade -c -o -v -k -f -T $BACKUPFILE $FILENAME; else cat error.log; fi; else echo "sum is not correct"; fi
+sha256sum -c $SHA256SUMS 2>/dev/null | if grep OK; then sysupgrade -T -f $BACKUPFILE $FILENAME 2>&1 > error.log; if [[ $? -eq 0 ]]; then sysupgrade -c -o -v -k -f $BACKUPFILE $FILENAME; else cat error.log; fi; else echo "sum is not correct"; fi
