547841f0047709986d1de3ceb0e5abe102e9d3e7
dmuiX <store@posteo.de>
2022-03-15 05:48:37 +0100

    Update AfterUpgradeInstallSteps.sh

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -1,16 +1,20 @@
-opkg update
-opkg install luci-compat luci-lib-ipkg
+
+# not sure so far if this is really needed:
 # opkg list-installed | grep -q uclient-fetch || opkg install uclient-fetch
 # opkg list-installed | grep -q libustream || opkg install libustream-mbedtls
-# luci-app-sqm
 
-cd /tmp
-read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk]"
-LUCI_APP_ARGON_CONFIG_LINK=${LUCI_APP_ARGON_CONFIG_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk}
-LUCI_APP_ARGON_CONFIG_FILENAME=$(echo $LUCI_APP_ARGON_CONFIG_LINK | cut -d/ -f9)
+# for devices with enough storage its possible to install an config app for the argon theme:
+# opkg install luci-app-sqm
+# cd /tmp
+# read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk]"
+# LUCI_APP_ARGON_CONFIG_LINK=${LUCI_APP_ARGON_CONFIG_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk}
+# LUCI_APP_ARGON_CONFIG_FILENAME=$(echo $LUCI_APP_ARGON_CONFIG_LINK | cut -d/ -f9)
 
-wget --no-check-certificate $LUCI_APP_ARGON_CONFIG_LINK -O $LUCI_APP_ARGON_CONFIG_FILENAME
-opkg install $LUCI_APP_ARGON_CONFIG_FILENAME
+# wget --no-check-certificate $LUCI_APP_ARGON_CONFIG_LINK -O $LUCI_APP_ARGON_CONFIG_FILENAME
+# opkg install $LUCI_APP_ARGON_CONFIG_FILENAME
+
+opkg update
+opkg install luci-compat luci-lib-ipkg
 
 rm $LUCI_APP_ARGON_CONFIG_FILENAME 
 
