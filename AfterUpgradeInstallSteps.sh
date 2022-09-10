d0e41e9a4d72fd13d87a31dc5096aa153ab7f010
dmuiX <store@posteo.de>
2022-09-10 04:21:26 +0200

    Update AfterUpgradeInstallSteps.sh
    switched the order of the installation of argon app config and argon theme.

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -6,12 +6,6 @@
 # for devices with enough storage its possible to install an config app for the argon theme:
 # opkg install luci-app-sqm
 cd /tmp
-read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk]"
-LUCI_APP_ARGON_CONFIG_LINK=${LUCI_APP_ARGON_CONFIG_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk}
-LUCI_APP_ARGON_CONFIG_FILENAME=$(echo $LUCI_APP_ARGON_CONFIG_LINK | cut -d/ -f9)
-
-wget --no-check-certificate $LUCI_APP_ARGON_CONFIG_LINK -O $LUCI_APP_ARGON_CONFIG_FILENAME
-opkg install $LUCI_APP_ARGON_CONFIG_FILENAME
 
 opkg update
 opkg install luci-compat luci-lib-ipkg
@@ -26,3 +20,10 @@ wget --no-check-certificate $LUCI_ARGON_THEME_LINK -O $LUCI_ARGON_THEME_FILENAME
 opkg install $LUCI_ARGON_THEME_FILENAME
 
 rm $LUCI_ARGON_THEME_FILENAME
+
+read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk]"
+LUCI_APP_ARGON_CONFIG_LINK=${LUCI_APP_ARGON_CONFIG_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9/luci-app-argon-config_0.9-20210309_all.ipk}
+LUCI_APP_ARGON_CONFIG_FILENAME=$(echo $LUCI_APP_ARGON_CONFIG_LINK | cut -d/ -f9)
+
+wget --no-check-certificate $LUCI_APP_ARGON_CONFIG_LINK -O $LUCI_APP_ARGON_CONFIG_FILENAME
+opkg install $LUCI_APP_ARGON_CONFIG_FILENAME
