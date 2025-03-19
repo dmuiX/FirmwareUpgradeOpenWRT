aa0de429b0176db28162a4c4d9fc93ee29880b63
Daniel Ullrich <19862760+dmuiX@users.noreply.github.com>
2025-03-20 00:20:48 +0100

    Update AfterUpgradeInstallSteps.sh

--- AfterUpgradeInstallSteps.sh
+++ AfterUpgradeInstallSteps.sh
@@ -3,7 +3,7 @@
 opkg update
 opkg install luci-compat luci-lib-ipkg
 
-read -p "Enter the download link of the luci argon theme [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.1/luci-theme-argon_2.3.1_all.ipk]"
+read -p "Enter the download link of the luci argon theme [https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk]"
 LUCI_ARGON_THEME_LINK=${LUCI_ARGON_THEME_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk}
 LUCI_ARGON_THEME_FILENAME=$(echo $LUCI_ARGON_THEME_LINK | cut -d/ -f9)
 
@@ -14,7 +14,7 @@ rm $LUCI_ARGON_THEME_FILENAME
 
 # for devices with enough storage its possible to install an config app for the argon theme:
 
-read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.9/luci-app-argon-config_0.9_all.ipk]"
+read -p "Enter the download link of the luci app argon config [https://github.com/jerrykuku/luci-theme-argon/releases/download/v1.8.4/luci-app-argon-config_0.9-20220424_all.ipk]"
 LUCI_APP_ARGON_CONFIG_LINK=${LUCI_APP_ARGON_CONFIG_LINK:-https://github.com/jerrykuku/luci-theme-argon/releases/download/v1.8.4/luci-app-argon-config_0.9-20220424_all.ipk}
 LUCI_APP_ARGON_CONFIG_FILENAME=$(echo $LUCI_APP_ARGON_CONFIG_LINK | cut -d/ -f9)
 
