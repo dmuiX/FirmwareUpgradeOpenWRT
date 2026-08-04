46ad3e070d7841b0796b2ec9e7efae455e36607f
Daniel Ullrich <19862760+dmuiX@users.noreply.github.com>
2026-08-05 01:12:50 +0200

    Fix installation checks for Argon theme and config

--- Install-LuCI-Argon-Theme.sh
+++ Install-LuCI-Argon-Theme.sh
@@ -7,7 +7,7 @@ DEFAULT_THEME_LINK="https://github.com/jerrykuku/luci-theme-argon/releases/downl
 DEFAULT_ARGON_CONFIG_LINK="https://github.com/jerrykuku/luci-app-argon-config/releases/download/v2.4.6/luci-app-argon-config-2.4.6-r1.apk"
 
 # Skip if already installed
-if apk info -e luci-theme-argon 2>/dev/null; then
+if apk info -e luci-theme-argon 2>/dev/null; then # must ask for version
   echo "LuCI Argon theme already installed."
 else
   read -p "Argon theme download link [${DEFAULT_THEME_LINK}]: LINK"
@@ -24,7 +24,7 @@ else
 fi
 
 # Install Argon Theme Config App
-if apk info -e luci-app-argon-config; 2>/dev/null; then
+if apk info -e luci-app-argon-config; 2>/dev/null; then #same version is missing
   echo "LuCI Argon config already installed."
 else
   read -p "Argon config download link [DEFAULT_ARGON_CONFIG_LINK]: LINK"
