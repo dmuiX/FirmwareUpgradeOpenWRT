0e811c87f780587f15b08d7514d8ac52ee7aa4b2
Daniel Ullrich <19862760+dmuiX@users.noreply.github.com>
2026-08-05 09:42:43 +0200

    chore: Clean up installation script by removing comments
    Removed commented code for installation checks and prompts.

--- Install-LuCI-Argon-Theme.sh
+++ Install-LuCI-Argon-Theme.sh
@@ -6,10 +6,6 @@ cd /tmp
 DEFAULT_THEME_LINK="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.4.6/luci-theme-argon-2.4.6-r1.apk"
 DEFAULT_ARGON_CONFIG_LINK="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.4.6/luci-app-argon-config-2.4.6-r1.apk"
 
-# Skip if already installed
-#if apk info -e luci-theme-argon 2>/dev/null; then # must ask for version
-#  echo "LuCI Argon theme already installed."
-#else
 read -p "Argon theme download link [${DEFAULT_THEME_LINK}]"
 LINK=${LINK:-$DEFAULT_THEME_LINK}
 FILE=$(basename "$LINK")
@@ -21,12 +17,7 @@ if ! apk info -e luci-theme-argon 2>/dev/null; then
   echo "Install failed"; exit 1
 fi
 echo "LuCI Argon theme installed."
-#fi
 
-# Install Argon Theme Config App
-#if apk info -e luci-app-argon-config; 2>/dev/null; then #same version is missing
-#  echo "LuCI Argon config already installed."
-#else
 read -p "Argon config download link [$DEFAULT_ARGON_CONFIG_LINK]"
 CONFIG_LINK=${CONFIG_LINK:-$DEFAULT_ARGON_CONFIG_LINK}
 CONFIG_FILE=$(basename "$CONFIG_LINK")
@@ -38,7 +29,6 @@ if ! apk info -e luci-app-argon-config 2>/dev/null; then
   echo "Install failed"; exit 1
 fi
 echo "LuCI Argon config app installed."
-#fi
 
 # Set Argon as active theme
 if [ "$(uci get luci.main.mediaurlbase 2>/dev/null)" != "/luci-static/argon" ]; then
